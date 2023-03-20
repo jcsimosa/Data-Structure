7e64826b588e2c421a9c7b8b0b4f74d294d2c8ca
Jose Simosa Nunez <josecarlossimosa@gmail.com>
2023-03-20 11:27:36 -1000

    insert method

--- CreateanArray.js
+++ CreateanArray.js
@@ -6,4 +6,15 @@ class Array {
     search(index){
         return this.array[index]
     }
+    push(data){
+        this.array[this.length] = data
+        this.length++
+        return this.data
+    }
 }
+
+let myArray = new Array()
+ myArray.push('apple')
+ myArray.push('grapes')
+
+ console.log(myArray)
