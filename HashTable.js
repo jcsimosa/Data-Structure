f363a8422b4e5496278d9510633938991de6ef9d
Jose Simosa Nunez <josecarlossimosa@gmail.com>
2023-03-21 12:51:08 -1000

    get value through the key

--- HashTable.js
+++ HashTable.js
@@ -19,13 +19,23 @@ class HashTable {
         return this.data
       }
       get(key){
-        let address = this._hash()
+        let address = this._hash(key);
+        let currentHolder = this.data[address]
+        if (currentHolder){
+            for( let i = 0; i < currentHolder.length; i++){
+                if (currentHolder[i][0] === key){
+                    return currentHolder[i][1]
+                }
+            }
+        }
+        return undefined
       }
+      
 }
 const myHashTable = new HashTable(50);
 myHashTable.insert("orange", 15)
 myHashTable.insert("grapes", 12)
 myHashTable.insert("apples", 20)
 
-console.log(myHashTable)
+console.log(myHashTable.get('apples'))
 
