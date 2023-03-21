dacb44bf9c62f9ab2cf97623016d7ffa26ec7547
Jose Simosa Nunez <josecarlossimosa@gmail.com>
2023-03-21 10:55:17 -1000

    insert method

--- HashTable.js
+++ HashTable.js
@@ -1,13 +1,31 @@
 class HashTable {
-    contructor(size){
-        this.data = new Array(size)
+    constructor(size){
+        this.data = new Array(size);
     }
-    _key(key){
-        let hash = 0
-        for (let i = 0; i < key.length; i++){
-            hash = (hash + key.chartCodeAt(i) * i) % this.data.length
-        } 
-        return hash    
-    }
-    
+
+    _hash(key) {
+        let hash = 0;
+        for (let i =0; i < key.length; i++){
+            hash = (hash + key.charCodeAt(i) * i) % this.data.length
+        }
+        return hash;
+      }
+      insert(key, value){
+        let address = this._hash(key);
+        if (!this.data[address]){
+            this.data[address] = []
+        }
+        this.data[address].push([key,value])
+        return this.data
+      }
+      get(key){
+        let address = this._hash()
+      }
 }
+const myHashTable = new HashTable(50);
+myHashTable.insert("orange", 15)
+myHashTable.insert("grapes", 12)
+myHashTable.insert("apples", 20)
+
+console.log(myHashTable)
+
