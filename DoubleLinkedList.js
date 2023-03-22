2aaee293ea4053dcfd110e1b0fceca24984258c5
Jose Simosa Nunez <josecarlossimosa@gmail.com>
2023-03-22 10:58:21 -1000

    preppend

--- DoubleLinkedList.js
+++ DoubleLinkedList.js
@@ -14,17 +14,31 @@ class DoubleLinkedList {
             next : null,
             prev : null
         }
-        let holdingNode = this.tail
-        newNode.prev = holdingNode
+        newNode.prev = this.tail
         this.tail.next = newNode
         this.tail = newNode
         this.length++
         return this     
     }
+    preppend(value){
+        let newNode = {
+            value : value,
+            next : null,
+            prev : null
+        }
+        newNode.next = this.head
+        this.head.prev = newNode
+        this.head = newNode
+        this.length++
+        return this 
+    }
+    
 }
 
 let myDoubleLinkedList = new DoubleLinkedList(10)
 myDoubleLinkedList.append(20)
 myDoubleLinkedList.append(30)
+myDoubleLinkedList.preppend(5)
+myDoubleLinkedList.preppend(1)
 
 console.log(myDoubleLinkedList)
