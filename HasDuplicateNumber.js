504647db49d011b49252ace87e91a0655feffb4e
Jose Simosa Nunez <josecarlossimosa@gmail.com>
2023-03-27 18:49:55 -1000

    hasDuplicate with O(n)

--- HasDuplicateNumber.js
+++ HasDuplicateNumber.js
@@ -12,7 +12,19 @@ function hasDuplicate(array) {
 console.log(numSteps)
 return false
 }
+function hasDuplicate2(array){
+    let arr = []
+    for (let i = 0; i < array.length; i++){
+        console.log(i)
+        if (arr[array[i]] === 1){
+            return arr
+        } else {
+            arr[array[i]] = 1
+        }
+    }
+    return false
+}
 
 let array = [3,5,8,10,10]
 
-console.log(hasDuplicate(array))
+console.log(hasDuplicate2(array))
