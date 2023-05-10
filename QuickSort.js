838b71b7952838b11dd376d4a99a3163d5b919f9
Jose Simosa Nunez <josecarlossimosa@gmail.com>
2023-05-10 11:56:59 -1000

    quickSort with better space complexity

--- QuickSort.js
+++ QuickSort.js
@@ -26,4 +26,44 @@ function quickSort(items){
 console.log(quickSort(numbers))
 
 
+function quickSort2(array, left, right){
+    const len = array.length; 
+    let pivot;
+    let partitionIndex;
+  
+    if(left < right) {
+      pivot = right;
+      partitionIndex = partition(array, pivot, left, right);
+      
+      //sort left and right
+      quickSort2(array, left, partitionIndex - 1);
+      quickSort2(array, partitionIndex + 1, right);
+    }
+    return array;
+  }
+     
+  function partition(array, pivot, left, right){
+    let pivotValue = array[pivot];
+    let partitionIndex = left;
+  
+    for(let i = left; i < right; i++) {
+      if(array[i] < pivotValue){
+        swap(array, i, partitionIndex);
+        partitionIndex++;
+      }
+    }
+    swap(array, right, partitionIndex);
+    return partitionIndex;
+  }
+  
+  function swap(array, firstIndex, secondIndex){
+      var temp = array[firstIndex];
+      array[firstIndex] = array[secondIndex];
+      array[secondIndex] = temp;
+  }
+  
+  //Select first and last index as 2nd and 3rd parameters
+  quickSort2(numbers, 0, numbers.length - 1);
+  console.log(numbers);
+
 
